235e1000e62709e12414d17fa722b0954dc1bdd9
adityanaidu20690 <47101245+adityanaidu20690@users.noreply.github.com>
2023-02-03 20:56:42 +0530

    Update angu.sh

--- angu.sh
+++ angu.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 git --version
 #curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash - &&
-sudo yum install nodejs -y
-node -v
-npm install -g @angular/cli
-ng new addy
+#sudo yum install nodejs -y
+#node -v
+#npm install -g @angular/cli
+#ng new addy
+cp -rvp /root/addy .
 cd addy/
 touch Dockerfile
 cat<<EOT>> Dockerfile
