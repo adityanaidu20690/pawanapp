53d291c32a02c80b8b54cc227f7ac24d3a56f424
adityanaidu20690 <47101245+adityanaidu20690@users.noreply.github.com>
2023-02-05 00:58:03 +0530

    Update angu.sh

--- angu.sh
+++ angu.sh
@@ -4,32 +4,6 @@ git --version
 #sudo yum install nodejs -y
 #node -v
 #npm install -g @angular/cli
-#ng new addy
+ng new addy
 #cp -rvp /root/addy .
 cd addy/
-touch Dockerfile
-cat<<EOT>> Dockerfile
-# Use the Node.js base image
-FROM node:14
-
-# Set the working directory
-WORKDIR /app
-
-# Copy the package.json and package-lock.json files
-COPY package*.json ./
-
-# Install the dependencies
-RUN npm install
-
-# Copy the rest of the files
-COPY . .
-
-# Build the Angular application
-RUN npm run build
-
-# Expose the port on which the application will run
-EXPOSE 4200
-
-# Start the application
-CMD ["npm", "start"]
-EOT
